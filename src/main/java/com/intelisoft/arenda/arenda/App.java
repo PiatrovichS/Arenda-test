ed860ba711ddbe39cea4bb212366c18e3c6a2efa
PiatrovichS <piatrovichs@gmail.com>
2017-07-18 22:01:26 +0300

    start new project

---
+++ src/main/java/com/intelisoft/arenda/arenda/App.java
@@ -0,0 +1,13 @@
+package com.intelisoft.arenda.arenda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
